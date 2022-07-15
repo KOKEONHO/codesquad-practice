38096507ebed7468f1a6d93f55aa51924f119d13
rhrjs <rhrjs@172.30.1.29>
2022-07-15 17:54:07 +0900

    1차 작성 완료
    프로그래밍 구현 제약사항 중 하나인 indent depth가 2까지만 허용되므로 이에 맞춰 우선적으로 제대로 기능하게 작성함
    (리팩토링 필요)

--- src/practice/NumberBaseballGame.java
+++ src/practice/NumberBaseballGame.java
@@ -13,31 +13,96 @@ public class NumberBaseballGame {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		// 서로 다른 3개의 숫자로 이루어진 임의의 세자릿 수 정하기 (100 ~ 999)
+		String answer = createRandomNumber();
 		boolean check = true;
-		Random rand = new Random();
-		int answer = rand.nextInt(899) + 100; // 난수 세자릿수 발생시킴 (100 ~ 999)
-		int user;
-
 		while (check) {
 			bw.write("숫자를 입력해주세요 ex)123 : ");
 			bw.flush();
-			user = Integer.parseInt(br.readLine()); // 유저 숫자 입력
-			checkUser(bw, answer, user, check); // 체크 메소드로 넘기기
-			bw.flush();
-		}
-		// 일치하면 반복 종료
-		bw.write("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
 
+			// 유저 숫자 입력
+			String user = br.readLine();
+			int strikeCnt = strikeCheck(answer, user);
+			int ballCnt = ballCheck(answer, user);
+
+			if (strikeCnt != 0) {
+				bw.write(strikeCnt + " 스트라이크 ");
+			}
+			if (ballCnt != 0) {
+				bw.write(ballCnt + " 볼");
+			}
+			if (strikeCnt == 0 && ballCnt == 0) {
+				bw.write("낫싱");
+			}
+			bw.newLine();
+			if (strikeCnt == 3) {
+				break;
+			}
+		}
+		bw.write("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 		bw.flush();
 		bw.close();
 
 	}
 
-	public static void checkUser(BufferedWriter bw, int answer, int user, boolean check) throws IOException {
+	public static String createRandomNumber() {
 
-		String strAns = String.valueOf(answer);
-		String strUsr = String.valueOf(user);
-		
-		
+		StringBuilder sb = new StringBuilder();
+		Random rand = new Random();
+		int number;
+
+		for (int i = 0; i < 3; i++) {
+			number = rand.nextInt(9) + 1;
+			if (!sb.toString().contains(String.valueOf(number))) {
+				sb.append(String.valueOf(number));
+			} else
+				i--;
+		}
+		return sb.toString();
+	}
+
+	public static int strikeCheck(String answer, String user) {
+
+		int strikeCnt = 0;
+		for (int i = 0; i < user.length(); i++) {
+			if (containsSameIdxCheck(answer, user, i)) {
+				strikeCnt++;
+			}
+		}
+		return strikeCnt;
+	}
+
+	public static boolean containsSameIdxCheck(String answer, String user, int index) {
+		if (answer.contains(Character.toString(user.charAt(index)))) {
+			if (answer.indexOf(user.charAt(index)) == index) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	public static int ballCheck(String answer, String user) {
+		int ballCnt = 0;
+		for (int i = 0; i < user.length(); i++) {
+			if (containsNotSameIdxCheck(answer, user, i)) {
+				ballCnt++;
+			}
+		}
+		return ballCnt;
+	}
+
+	public static boolean containsNotSameIdxCheck(String answer, String user, int index) {
+		if (answer.contains(Character.toString(user.charAt(index)))) {
+			if (answer.indexOf(user.charAt(index)) != index) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
 	}
 }
